a42d857d9d3e428228169e7d3f7101023aeb3c98
Kevin Stradtman <stradtkt22@gmail.com>
2019-12-20 23:20:14 -0500

    Added some styles and added search to the page, search will be complete in further commits

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+			<?php astra_content_bottom(); ?>
+			</div> <!-- ast-container -->
+		</div><!-- #content -->
+		<?php astra_content_after(); ?>
+		<?php astra_footer_before(); ?>
+		<?php astra_footer(); ?>
+		<?php astra_footer_after(); ?>
+	</div><!-- #page -->
+	<?php astra_body_bottom(); ?>
+    <div class="search-overlay">
+        <div class="search-overlay__top">
+            <div class="container">
+                <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                <input type="text" placeholder="What are you looking for?" class="search-term" id="search-term">
+                <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    <?php wp_footer(); ?>
+	</body>
+</html>
--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 function wp_business_register_scripts() {
     wp_enqueue_style('astra-child-styles', get_stylesheet_uri());
     wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css');
-    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.4.1.min.js');
+    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.4.1.min.js', NULL, '3.4.1', true);
+    wp_enqueue_script('custom-js', get_theme_file_uri('/js/scripts.js'), array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'wp_business_register_scripts');
