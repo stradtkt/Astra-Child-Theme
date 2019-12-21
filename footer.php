5ab4e236a23dca2e585205c9633ab470b7528a6b
Kevin Stradtman <stradtkt22@gmail.com>
2019-12-21 18:45:57 -0500

    Added more to search

--- footer.php
+++ footer.php
@@ -27,9 +27,12 @@ if ( ! defined( 'ABSPATH' ) ) {
         <div class="search-overlay__top">
             <div class="container">
                 <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                <input type="text" placeholder="What are you looking for?" class="search-term" id="search-term">
+                <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
                 <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
             </div>
+        </div>
+        <div class="container">
+            <div id="search-overlay__results"></div>
         </div>
     </div>
     <?php wp_footer(); ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,40 @@
 (function($) {
    class Search {
         constructor() {
+            this.resultsDiv = $("#search-overlay__results");
             this.openButton = $(".js-search-trigger");
             this.closeButton = $(".search-overlay__close");
             this.searchOverlay = $(".search-overlay");
+            this.searchField = $("#search-term");
             this.events();
             this.isOverlayOpen = false;
+            this.isSpinnerVisable = false;
+            this.previousValue;
+            this.typingTimer;
         }
 
         events() {
             this.openButton.on('click', this.openOverlay.bind(this));
             this.closeButton.on('click', this.closeOverlay.bind(this));
             $(document).on("keydown", this.keyPressDispatcher.bind(this));
+            this.searchField.on("keyup", this.typingLogic.bind(this));
+        }
+
+        typingLogic() {
+            if(this.searchField.val() != this.previousValue) {
+                clearTimeout(this.typingTimer);
+                if(!this.isSpinnerVisable) {
+                    this.resultsDiv.html('<div class="spinner-loader"></div>');
+                    this.isSpinnerVisable = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+            }
+            this.previousValue = this.searchField.val();
+        }
+
+        getResults() {
+            this.resultsDiv.html("Imagine real search results here");
+            this.isSpinnerVisable = false;
         }
 
         keyPressDispatcher(e) {
